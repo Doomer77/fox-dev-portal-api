5980a82adaece67132f24be4d5644d7b5b253169
Doomer77 <lfa1925@rambler.ru>
2024-02-05 22:18:12 +0300

    add articles entity

--- src/user/middlewares/auth.middleware.ts
+++ src/user/middlewares/auth.middleware.ts
@@ -1,5 +1,5 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { ExpressRequestInterface } from '@app/types/expressRequest.interface';
 import { verify } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
